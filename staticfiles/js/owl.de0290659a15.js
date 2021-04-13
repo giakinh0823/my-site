6fb01da474b66e280b3b84e72ca05f231195110b
Kính Hà Gia <giakinh2000@gmail.com>
2021-04-14 05:15:41 +0700

    static file

---
+++ staticfiles/js/owl.de0290659a15.js
@@ -0,0 +1,11 @@
+
+    $(document).ready(function() {
+        $("#owl-demo").owlCarousel({
+          navigation : true,
+          items : 1, 
+          loop:true,
+            nav:false,
+            // animateOut: 'fadeOut',
+            // animateIn: 'fadeIn',
+        });
+      });
